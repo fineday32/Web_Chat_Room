8afb6df5113a36782f10a21a1143dbe13f5be60b
fineday32 <hcythomas0125@gmail.com>
2015-08-26 15:02:50 -0700

    socket.io 2

--- webChat/app.js
+++ webChat/app.js
@@ -16,11 +16,17 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
   console.log('a user connected');
-})
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 http.listen(3000, function(){
   console.log('listening on *: 3000');
-})
+});
 
 
 var routes = require('./routes/index');
