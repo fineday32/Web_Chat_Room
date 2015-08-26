d81f4e95d5381c9669b4331baa6afd743e8da7d8
fineday32 <hcythomas0125@gmail.com>
2015-08-26 15:19:36 -0700

    socket.io transfer to client side directly

--- webChat/app.js
+++ webChat/app.js
@@ -18,6 +18,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
